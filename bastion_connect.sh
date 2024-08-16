bd7073e087b89b112b50091d24a63803f27d7ad8
tals <tal.silbiger@harmonicinc.com>
2024-08-16 11:50:21 +0300

    changed to exlusive path

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -17,6 +17,7 @@ PUBLIC_IP=$1
 PRIVATE_IP=$2
 COMMAND=$3
 
+
 # Construct the SSH command
 if [ -z "$PRIVATE_IP" ]; then
     # Case 2: Connect to the public instance
@@ -25,9 +26,9 @@ else
     # Case 1 & Case 3: Connect to the private instance via the public instance
     if [ -z "$COMMAND" ]; then
         # Case 1: Interactive SSH to private instance
-        ssh -i "$KEY_PATH" -t ubuntu@"$PUBLIC_IP" ssh -i "$KEY_PATH" ubuntu@"$PRIVATE_IP"
+        ssh -i "$KEY_PATH" -t ubuntu@"$PUBLIC_IP" ssh -i /home/ubuntu/key.pem ubuntu@"$PRIVATE_IP"
     else
         # Case 3: Run command on the private instance
-        ssh -i "$KEY_PATH" -t ubuntu@"$PUBLIC_IP" ssh -i "$KEY_PATH" ubuntu@"$PRIVATE_IP" "$COMMAND"
+        ssh -i "$KEY_PATH" -t ubuntu@"$PUBLIC_IP" ssh -i /home/ubuntu/key.pem ubuntu@"$PRIVATE_IP" "$COMMAND"
     fi
 fi
