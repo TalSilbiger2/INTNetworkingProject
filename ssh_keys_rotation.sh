d153e0a72f37bda4d4f8340d06eb627ce1413ad8
tals <tal.silbiger@harmonicinc.com>
2024-08-18 19:47:15 +0300

    changes

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -10,13 +10,18 @@ PRIVATE_IP="$1"
 #cp "$HOME/key.pem" "$HOME/.ssh/id_rsa_old"
 
 
-NEW_KEY_PATH="$HOME/.ssh/id_rsa"   # Path to store the new key
-OLD_KEY_PATH="$NEW_KEY_PATH"  # Path to your old key
-OLD_KEY_PATH_PUB="$NEW_KEY_PATH.pub"
+PRIVATE_KEY_PATH="$HOME/.ssh/id_rsa"   # Path to store the new key
+PUBLIC_KEY_PATH="$PRIVATE_KEY_PATH".pub
+
+OLD_PRIVATE_KEY_PATH="$PRIVATE_KEY_PATH".old  # Path to your old key
+mv "$PRIVATE_KEY_PATH" "$OLD_PRIVATE_KEY_PATH"
+
+OLD_KEY_PATH_PUB="$PUBLIC_KEY_PATH".old
+mv "$PUBLIC_KEY_PATH" "$OLD_KEY_PATH_PUB"
 
 # Generate a new SSH key pair
-ssh-keygen -t rsa -b 4096 -f "$NEW_KEY_PATH" -N ""  -C "Key rotated on $(date)" > /dev/null
-chmod 400 "$NEW_KEY_PATH"
+ssh-keygen -t rsa -b 4096 -f "$PRIVATE_KEY_PATH" -N ""  -C "Key rotated on $(date)" > /dev/null
+chmod 400 "$PRIVATE_KEY_PATH"
 
 # Ensure the old key exists before using it
 #if [ ! -f "$OLD_KEY_PATH" ]; then
@@ -24,22 +29,11 @@ chmod 400 "$NEW_KEY_PATH"
 #    exit 1
 #fi
 
-if ! ssh -i "$OLD_KEY_PATH" ubuntu@"$PRIVATE_IP" "cat > ~/.ssh/authorized_keys" < "$NEW_KEY_PATH.pub"; then
+if ! ssh -i "$OLD_PRIVATE_KEY_PATH" ubuntu@"$PRIVATE_IP" "cat > ~/.ssh/authorized_keys" < "$PUBLIC_KEY_PATH"; then
   echo "Failed to copy public key to the private machine"
   exit 1
 fi
 
-OLD_KEY_PATH="$NEW_KEY_PATH.old"  # Path to your old key
-OLD_KEY_PATH_PUB="$NEW_KEY_PATH.pub.old"
-
-#ssh -o StrictHostKeyChecking=no -i "$OLD_KEY_PATH" ubuntu@"$PRIVATE_IP" << EOF
-#mkdir -p ~/.ssh
-#chmod 700 ~/.ssh
-#cat > ~/.ssh/authorized_keys << EOL
-#$(cat "${NEW_KEY_PATH}.pub")
-#EOL
-#chmod 600 ~/.ssh/authorized_keys
-#EOF
 
 # Test SSH connection with the new key
 ssh -o StrictHostKeyChecking=no -i "$NEW_KEY_PATH" ubuntu@"$PRIVATE_IP"
